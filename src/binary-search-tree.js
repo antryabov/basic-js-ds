68bc44e24167d447daf5f48cd2c5ab5abc3da1a9
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-11-24 17:03:49 +0300

    feat: add method for adding in bst

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,45 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.treetop = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.treetop;
+  }
+
+  add(data) {
+    const addNode = new Node(data);
+
+    if (!this.treetop) {
+      this.treetop = addNode;
+      return;
+    }
+    let nodeTree = this.treetop;
+
+    while (nodeTree) {
+      if (nodeTree.data > data) {
+        if (!nodeTree.left) {
+          nodeTree.left = addNode;
+          break;
+        }
+        nodeTree = nodeTree.left;
+      }
+      if (nodeTree.data < data) {
+        if (!nodeTree.right) {
+          nodeTree.right = addNode;
+          break;
+        }
+        nodeTree = nodeTree.right;
+      }
+    }
   }
 
   has(/* data */) {
