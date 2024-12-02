9296ec68f3121584bab8dba9f3e651f777f78f3e
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-12-02 15:12:37 +0300

    feat: solve task removeFromList

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let linkedList = l;
+
+  if (!linkedList) {
+    return null;
+  }
+
+  if (linkedList.value === k) {
+    linkedList = linkedList.next
+  }
+  let current = linkedList.next
+
+
+
+  if (current !== null) {
+    while (current.next) {
+      if (current.next.value === k) {
+        current.next = current.next.next
+      } else {
+        current = current.next
+      }
+    }
+  }
+
+
+
+  return linkedList
 }
 
 module.exports = {
