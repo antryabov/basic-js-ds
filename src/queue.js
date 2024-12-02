a540cefb3fcdc2f42c07038320decd9c75a527d2
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-12-02 16:05:53 +0300

    feat: rebuild the queue data structure through a linked list

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -15,20 +15,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Queue {
   constructor() {
-    this.queue = [];
+    this.queue = null;
   }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue
   }
 
   enqueue(value) {
-    this.queue.unshift(value);
+    const newNode = new ListNode(value);
+
+    if (!this.queue) {
+      this.queue = newNode
+    } else {
+      let current = this.queue
+      while (current.next) {
+        current = current.next
+      }
+      if (!current.next) {
+        current.next = newNode
+      }
+    }
+
+
   }
 
   dequeue() {
-    return this.queue.pop();
+    let removeNode = this.queue
+    this.queue = this.queue.next
+    return removeNode.value
   }
 }
 
